e208dd9556097756009fbbb1d22d23a0cae7f869
equescodebelike <nep3600@gmail.com>
2025-05-15 11:23:55 +0300

    asset

--- lib/feature/choose.dart
+++ lib/feature/choose.dart
@@ -100,40 +100,40 @@ class _ChooseState extends State<Choose> {
   Widget PdfWidget() {
     switch (widget.title) {
       case 'ШИФР ЦЕЗАРЯ':
-        return SfPdfViewer.network(
-          'https://github.com/equescodebelike/ciphers/raw/refs/heads/master/assets/symetric/%D0%A8%D0%B8%D1%84%D1%80_%D0%A6%D0%B5%D0%B7%D0%B0%D1%80%D1%8F.pdf',
+        return SfPdfViewer.asset(
+          'symetric/caesar_cipher.pdf',
         );
       case 'КЛЮЧЕВОЙ ШИФР':
-        return SfPdfViewer.network(
-          'https://github.com/equescodebelike/ciphers/raw/refs/heads/master/assets/symetric/%D0%9A%D0%BB%D1%8E%D1%87%D0%B5%D0%B2%D0%BE%D0%B9_%D1%88%D0%B8%D1%84%D1%80.pdf',
+        return SfPdfViewer.asset(
+          'symetric/key_cipher.pdf',
         );
       case 'ШИФР ВИЖЕНЕРА':
-        return SfPdfViewer.network(
-          'https://github.com/equescodebelike/ciphers/raw/refs/heads/master/assets/symetric/%D1%88%D0%B8%D1%84%D1%80_%D0%92%D0%B8%D0%B6%D0%B5%D0%BD%D0%B5%D1%80%D0%B0.pdf',
+        return SfPdfViewer.asset(
+          'symetric/vigenere_cipher.pdf',
         );
       case 'ШИФР ПЛЕЙФЕРА':
-        return SfPdfViewer.network(
-          'https://github.com/equescodebelike/ciphers/raw/refs/heads/master/assets/symetric/%D1%88%D0%B8%D1%84%D1%80_%D0%9F%D0%BB%D0%B5%D0%B9%D1%84%D0%B5%D1%80%D0%B0.pdf',
+        return SfPdfViewer.asset(
+          'symetric/playfair_cipher.pdf',
         );
       case 'ШИФР РЕШЕТКИ':
-        return SfPdfViewer.network(
-          'https://github.com/equescodebelike/ciphers/raw/refs/heads/master/assets/symetric/%D1%88%D0%B8%D1%84%D1%80_%D1%80%D0%B5%D1%88%D0%B5%D1%82%D0%BA%D0%B8.pdf',
+        return SfPdfViewer.asset(
+          'symetric/grid_cipher.pdf',
         );
       case 'ШИФР ДИФФИ-ХЕЛЛМАНА':
-        return SfPdfViewer.network(
-          'https://raw.githubusercontent.com/equescodebelike/ciphers/refs/heads/master/assets/assymetric/%D0%A8%D0%B8%D1%84%D1%80%20%D0%94%D0%B8%D1%84%D1%84%D0%B8-%D0%A5%D0%B5%D0%BB%D0%BB%D0%BC%D0%B0%D0%BD%D0%B0.pdf',
+        return SfPdfViewer.asset(
+          'assymetric/diffie_hellman_cipher.pdf',
         );
       case 'ШИФР ЭЛЬ-ГАМАЛЯ':
-        return SfPdfViewer.network(
-          'https://raw.githubusercontent.com/equescodebelike/ciphers/refs/heads/master/assets/assymetric/%D0%A8%D0%B8%D1%84%D1%80%20%D0%AD%D0%BB%D1%8C-%D0%93%D0%B0%D0%BC%D0%B0%D0%BB%D1%8F%20%D1%81%D0%BA%D0%B0%D0%BD.pdf',
+        return SfPdfViewer.asset(
+          'assymetric/el_gamal_cipher.pdf',
         );
       case 'ШИФР RSA':
-        return SfPdfViewer.network(
-          'https://raw.githubusercontent.com/equescodebelike/ciphers/b2b2aca1cc004dea71c549e88284ecdb128882c2/assets/assymetric/%D1%88%D0%B8%D1%84%D1%80_RSA.pdf',
+        return SfPdfViewer.asset(
+          'assymetric/rsa_cipher.pdf',
         );
       default:
-        return SfPdfViewer.network(
-          'https://cdn.syncfusion.com/content/PDFViewer/flutter-succinctly.pdf',
+        return SfPdfViewer.asset(
+          'symetric/caesar_cipher.pdf',
         );
     }
   }
